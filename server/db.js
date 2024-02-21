2d9cb14a9a15a91da48643edc8c2baebe37e9e84
Harsh Butani <harshbutani2004@gmail.com>
2024-02-21 21:42:26 +0530

    Account Schema Added in db file

--- server/db.js
+++ server/db.js
@@ -1,17 +1,51 @@
 const mongoose = require('mongoose');
+const { Schema, number } = require('zod');
 
 mongoose.connect('mongodb+srv://harshbutani125:LVieEHRKoS1P3j1F@cluster0.fktefhx.mongodb.net/Paytm');
 
 const userSchema = new mongoose.Schema({
-    username: String,
-    firstname: String,
-    lastname: String,
-    password: Number
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minLength: 3,
+        maxLength: 30,
+        lowercase: true,
+        trim: true,
+
+    },
+    firstname: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastname: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    }
+});
+
+const accountSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', require: true },
+    balance: {
+        type: Number,
+        required: true
+    }
 });
 
 
 const User = mongoose.model('user', userSchema);
+const Account = mongoose.model('Account', accountSchema);
 
 module.exports = {
-    User
+    User,
+    Account
 };
